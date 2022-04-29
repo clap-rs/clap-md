f39fa6e8da2b0aaff2784ccb43643244bcfe2554
asrosent <asrosent@microsoft.com>
2022-04-29 08:15:12 +0300

    upgrade crates

--- examples/testapp.rs
+++ examples/testapp.rs
@@ -3,19 +3,18 @@
 extern crate clap;
 extern crate clap_md;
 
-use clap::{App, AppSettings, Arg, SubCommand};
+use clap::{Arg, Command};
 use clap_md::app_to_md;
 
 fn main() {
-    let a = App::new("testapp")
+    let a = Command::new("testapp")
     .about("Pointless application")
-    .setting(AppSettings::SubcommandRequiredElseHelp)
+    .subcommand_required(true)
     .author("Katharina Fey <kookie@spacekookie.de>")
-    // .author("Yosh Wuyts <y@w.s")
     .long_about("Lorem Ipsum bla bla bla")
-    .arg(Arg::with_name("debug").short("d").help("Make program output debug messages"))
-    .arg(Arg::with_name("output").short("o").takes_value(true).help("Output File"))
-    .subcommand(SubCommand::with_name("foo").arg(Arg::with_name("bar").short("b").long("barr")));
+    .arg(Arg::new("debug").short('d').help("Make program output debug messages"))
+    .arg(Arg::new("output").short('o').takes_value(true).help("Output File"))
+    .subcommand(Command::new("foo").arg(Arg::new("bar").short('b').long("barr")));
 
     let markdown = app_to_md(&a, 1).unwrap();
     println!("{}", markdown);
